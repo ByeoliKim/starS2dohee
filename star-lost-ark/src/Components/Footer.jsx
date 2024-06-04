743adf67e0ac9cfe1ef920377f8b193daa7d5cbe
Byeolikim <213069@naver.com>
2024-06-04 17:52:28 +0900

    ⭐ 레이아웃 CSS

--- star-lost-ark/src/Components/Footer.jsx
+++ star-lost-ark/src/Components/Footer.jsx
@@ -1,7 +1,10 @@
 export default function Footer() {
     return (
         <>
-            나는 푸터얌
+            <footer className="px-5 py-12 bg-[#656565]">
+                <h1 className="pb-5 text-xl font-bold tracking-tighter">⭐별로와⭐</h1>
+                <p className="text-sm">Smilegate RPG의 승인을 받지 않았으며, Smilegate RPG 또는 Lostark 제작 또는 관리에 공식적으로 관여한 모든 사람의 견해 또는 의견을 반영하지 않습니다. Lostark 및 Smilegate RPG는 Smilegate RPG, Inc. Lostark © Smilegate RPG, Inc.의 상표 또는 등록 상표입니다.</p>
+            </footer>
         </>
     )
 }
--- star-lost-ark/src/Components/Header.jsx
+++ star-lost-ark/src/Components/Header.jsx
@@ -1,7 +1,9 @@
 export default function Header() {
     return (
         <>
-            나는 헤더얌
+            <header className="bg-[#656565] p-5">
+                <h1 className="text-3xl font-thin tracking-tighter">⭐별로와 ⭐</h1>
+            </header>
         </>
     )
 }
