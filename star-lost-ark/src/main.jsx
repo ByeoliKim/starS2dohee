f4515c1e20ef885f2182f3af9c0cba21854a6865
Byeolikim <213069@naver.com>
2024-06-11 18:55:14 +0900

    ㅅㅌ

--- star-lost-ark/src/main.jsx
+++ star-lost-ark/src/main.jsx
@@ -3,13 +3,18 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { QueryClient, QueryClientProvider } from 'react-query'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+        </Routes>
+      </BrowserRouter>
     </QueryClientProvider>
   </React.StrictMode>,
 )
