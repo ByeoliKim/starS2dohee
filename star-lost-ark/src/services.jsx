e6a20e858442261d04e9ced0ff43199d37707ffd
Byeolikim <213069@naver.com>
2024-06-11 18:55:02 +0900

    장비 api 추가띠

--- star-lost-ark/src/services.jsx
+++ star-lost-ark/src/services.jsx
@@ -5,6 +5,8 @@ const url = `https://developer-lostark.game.onstove.com`;
 // eslint-disable-next-line no-undef
 const API_KEY = import.meta.env.VITE_API_KEY;
 
+
+// 프로필 내역
 export const getChaData = async (chaName) => {
     let data = await axios.get(url + `/armories/characters/${chaName}/profiles`, {
         headers: {
@@ -13,6 +15,21 @@ export const getChaData = async (chaName) => {
         },
     });
 
+    if (data.data === null) {
+        return null;
+    } else {
+        return data;
+    }
+}
+
+// 장비 내역
+export const getEquData = async (chaName) => {
+    let data = await axios.get(url + `/armories/characters/${chaName}/equipment`, {
+        headers: {
+            accept: 'application/json',
+            Authorization: `bearer ${API_KEY}`
+        }
+    })
     if (data.data === null) {
         return null;
     } else {
