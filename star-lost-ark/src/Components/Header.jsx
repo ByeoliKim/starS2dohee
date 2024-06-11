ea5965798157767d5901d7116412279e3e03a4aa
Byeolikim <213069@naver.com>
2024-06-11 18:54:52 +0900

    ㅅㅌ

--- star-lost-ark/src/Components/Header.jsx
+++ star-lost-ark/src/Components/Header.jsx
@@ -1,8 +1,14 @@
+import { Link } from "react-router-dom";
+
 export default function Header() {
     return (
         <>
             <header className="bg-[#656565] p-5">
-                <h1 className="text-3xl font-thin tracking-tighter">⭐별로와 ⭐</h1>
+                <h1 className="text-3xl font-thin tracking-tighterfont-Cafe24Shiningstar">
+                    <Link to="/">
+                    ⭐별로와 ⭐
+                    </Link>
+                </h1>
             </header>
         </>
     )
